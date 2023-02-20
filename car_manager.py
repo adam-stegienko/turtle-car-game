16954b1c57f542b406b3c0884284233a3c115dfe
Adam Stegienko <adam.stegienko1@gmail.com>
2023-02-20 14:55:13 +0100

    details prerequisite added

--- car_manager.py
+++ car_manager.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 
+COLORS = ["red", "green", "orange", "yellow", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
 class CarManager(Turtle):
     def __init__(self) -> None:
         super().__init__()
